ef96f8c3abfd693e1c602bdea23e82bb9317f61c
Rafael Nanes <rafaelbraga_22@hotmail.com>
2024-12-23 15:01:36 +0000

    added springboot test case

---
+++ springboot-test/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    application
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.3"
+    java
+}
+
+group = "com.sivalabs"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+application {
+    mainClass.set("com.sivalabs.bookmarks.Application") // Replace with your application's main class
+}
